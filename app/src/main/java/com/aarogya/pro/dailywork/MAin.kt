ccb9cea171655a71f4d7c9e4069398aaa8a5f88b
Vikas <coolram1114@gmail.com>
2019-08-29 10:59:23 +0530

    Initial commit

---
+++ app/src/main/java/com/aarogya/pro/dailywork/MAin.kt
@@ -0,0 +1,11 @@
+package com.aarogya.pro.dailywork
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MAin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
