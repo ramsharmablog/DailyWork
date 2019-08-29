184105405609e17a19a9731bf5b04388aef479e7
Vikas <coolram1114@gmail.com>
2019-08-29 11:05:53 +0530

    manifest

--- app/src/main/java/com/aarogya/pro/dailywork/MainActivity.java
+++ app/src/main/java/com/aarogya/pro/dailywork/MainActivity.java
@@ -3,9 +3,10 @@ package com.aarogya.pro.dailywork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
